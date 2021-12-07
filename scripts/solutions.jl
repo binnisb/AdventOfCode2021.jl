cdbae1cb47af52bbf4add5eb9aa6b48fb04625a5
Brynjar Smári Bjarnason <binni@binnisb.com>
2021-12-07 12:28:09 +0100

    update

--- scripts/solutions.jl
+++ scripts/solutions.jl
@@ -1,10 +1,15 @@
 using AOC2021
+using BenchmarkTools: @btime
 for i = 1:25
     for j = 1:2
         try
-            println("$i, $j: " ,solve(FullInput,i, j))
-        catch
-            continue
+            println("D:$i P:$j - ", solve(FullInput,i, j))
+        catch e
+            if isa(e, MethodError) || isa(e, SystemError)
+                continue
+            else
+                rethrow()
+            end
         end
     end
 end
